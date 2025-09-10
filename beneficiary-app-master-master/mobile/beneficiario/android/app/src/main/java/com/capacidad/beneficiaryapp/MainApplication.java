0d55f0b8d4bee8311d2f6d06e02c8b7dd80b3751
Lucas <galileo.laino@gmail.com>
2025-09-10 15:29:51 -0300

    feat: add hermes-engine dependency and configure android package name
    - Added "hermes-engine" version 0.11.0 to package.json for improved performance.
    - Updated react-native.config.js to specify the android package name as 'com.capacidad.beneficiaryapp'.
    - Created native_modules.gradle for manual autolinking configuration in React Native 0.81+.

--- beneficiary-app-master-master/mobile/beneficiario/android/app/src/main/java/com/capacidad/beneficiaryapp/MainApplication.java
+++ beneficiary-app-master-master/mobile/beneficiario/android/app/src/main/java/com/capacidad/beneficiaryapp/MainApplication.java
@@ -1,19 +1,15 @@
 package com.capacidad.beneficiaryapp;
 
 import android.app.Application;
-
 import android.content.Context;
-import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.google.android.gms.security.ProviderInstaller;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Arrays;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,9 +22,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      List<ReactPackage> packages = Arrays.<ReactPackage>asList(
+          // Minimal package list - modules will be discovered via manual settings.gradle
+      );
       return packages;
     }
 
@@ -48,13 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    try {
-      ProviderInstaller.installIfNeeded(getApplicationContext());
-    } catch (GooglePlayServicesRepairableException e) {
-      e.printStackTrace();
-    } catch (GooglePlayServicesNotAvailableException e) {
-      e.printStackTrace();
-    }
+    // Removed ProviderInstaller references to avoid Google Play Services dependency
   }
 
   /**
