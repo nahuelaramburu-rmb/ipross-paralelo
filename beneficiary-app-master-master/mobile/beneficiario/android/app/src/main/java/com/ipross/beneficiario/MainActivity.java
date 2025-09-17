7387045fd872d52a239646209e68b3d800366a67
Lucas <galileo.laino@gmail.com>
2025-09-17 19:17:59 -0300

    feat: Add MainActivity and MainApplication classes for React Native integration

---
+++ beneficiary-app-master-master/mobile/beneficiario/android/app/src/main/java/com/ipross/beneficiario/MainActivity.java
@@ -0,0 +1,40 @@
+package com.ipross.beneficiario;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+
+// Gesture Handler
+import android.os.Bundle;
+// import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "iprossvem";
+    }
+
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. We use a util class {@link
+     * ReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                ReactRootView reactRootView = new ReactRootView(MainActivity.this);
+                // ReactRootView reactRootView = new RNGestureHandlerEnabledRootView(MainActivity.this);
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+                return reactRootView;
+            }
+        };
+    }
+}
---
+++ beneficiary-app-master-master/mobile/beneficiario/android/app/src/main/java/com/ipross/beneficiario/MainApplication.java
@@ -0,0 +1,79 @@
+package com.ipross.beneficiario;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+
+// Gesture Handler
+// import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+
+// AsyncStorage
+// import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+
+// Firebase (commented for now)
+// import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = Arrays.<ReactPackage>asList(
+              new MainReactPackage()
+              // new RNGestureHandlerPackage(),
+              // new AsyncStoragePackage()
+              // new ReactNativeFirebaseAppPackage(),
+              // new ReactNativeFirebaseMessagingPackage()
+              // Add other packages here as needed for manual configuration
+          );
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      // We use reflection here to pick up the class that initializes the new architecture,
+      // since the new architecture is not available in old version of react native
+      try {
+        Class<?> newArchClass = Class.forName("com.facebook.react.NewArchBootstrapper");
+        newArchClass.getMethod("load").invoke(null);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    // COMMENTED OUT: ReactNativeFlipper is not included in release builds
+    // ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+  }
+
+}
